5c1b48010879f600f2fd5bcd4f8a0a075b16861d
Avik Das <adas@linkedin.com>
2011-06-17 13:20:58 -0700

    Non-nested "gets" supported

--- lib/asap.rb
+++ lib/asap.rb
@@ -1,9 +1,30 @@
 require 'asap/netty'
 
-def Asap
+require 'open-uri'
 
+def Asap &blk
+  context = Asap::FetchContext.new
+  context.instance_eval &blk
+  context.join
+  context.result
 end
 
 module Asap
+  class FetchContext
+    def initialize
+      @result = []
+      @index   = 0
+    end
 
+    def get url
+      @result[@index] = open(url) {|r| r.read}
+      @index += 1
+    end
+
+    def join
+      # no threading yet
+    end
+
+    attr_reader :result
+  end
 end
