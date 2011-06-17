a8a2c374f5d6e3a387f10f50095edff5ae74da0a
Greg Spurrier <gspurrier@linkedin.com>
2011-06-17 15:12:44 -0700

    Add em_test_server

---
+++ script/em_test_server.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'eventmachine'
+require 'evma_httpserver'
+
+class MyHttpServer < EM::Connection
+  include EM::HttpServer
+
+   def post_init
+     super
+     no_environment_strings
+   end
+
+  def process_http_request
+    # the http request details are available via the following instance variables:
+    #   @http_protocol
+    #   @http_request_method
+    #   @http_cookie
+    #   @http_if_none_match
+    #   @http_content_type
+    #   @http_path_info
+    #   @http_request_uri
+    #   @http_query_string
+    #   @http_post_content
+    #   @http_headers
+
+    empty, time, message = @http_path_info.split('/')
+
+    response = EM::DelegatedHttpResponse.new(self)
+    response.status = 200
+    response.content_type 'text/html'
+    response.content = message
+
+    EventMachine::Timer.new(time.to_i) do
+      response.send_response
+    end
+  end
+end
+
+EventMachine.run{
+  EventMachine.start_server '0.0.0.0', 1234, MyHttpServer
+}
