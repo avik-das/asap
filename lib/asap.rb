f79071c0bc82426e6f0df06fe03a726b6281fee0
Avik Das <adas@linkedin.com>
2011-06-17 13:38:15 -0700

    Added nested "get" support

--- lib/asap.rb
+++ lib/asap.rb
@@ -3,21 +3,35 @@ require 'asap/netty'
 require 'open-uri'
 
 def Asap &blk
-  context = Asap::FetchContext.new
-  context.instance_eval &blk
-  context.join
-  context.result
+  Asap.execute blk
 end
 
 module Asap
+  class << self
+    def execute blk, *args
+      context = Asap::FetchContext.new
+      context.instance_exec *args, &blk
+      context.join
+      context.result
+    end
+  end
+
   class FetchContext
     def initialize
       @result = []
       @index   = 0
     end
 
-    def get url
-      @result[@index] = open(url) {|r| r.read}
+    def get url, &blk
+      #@result[@index] = open(url) {|r| r.read}
+
+      res = open(url) {|r| r.read}
+      if block_given?
+        nested = Asap.execute blk, res
+        res = [res, nested]
+      end
+
+      @result[@index] = res
       @index += 1
     end
 
